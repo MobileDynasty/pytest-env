5b8e700b10343c827e788fec0c73cabc8202565c
Lucas <lbudman@cognilytics.com>
2015-10-14 10:49:58 -0600

    add default vaule setting that will not override existing env variables

--- pytest_env/plugin.py
+++ pytest_env/plugin.py
@@ -13,4 +13,15 @@ def pytest_load_initial_conftests(args, early_config, parser):
         part = e.partition("=")
         key = part[0].strip()
         value = part[2].strip()
-        os.environ[key] = value
+
+        # use D: as a way to designate a default value 
+        # that will only override env variables if they 
+        # do not exist already
+        dkey = key.split("D:")
+        default_val = False
+        if len(dkey) == 2:
+            key = dkey[1]
+            default_val = True
+
+        if not default_val or key not in os.environ:
+            os.environ[key] = value
